90ffd3c5f9ae2d18466e77d6dbeb63b28837d8f6
jousef1 <yousefel560@gmail.com>
2023-03-08 12:52:49 +0800

    login auth

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,33 +1,14 @@
-// import React from 'react'
-// import { auth, provider } from "../config/firebase";
-// import { signInWithPopup } from "firebase/auth";
-// import {useNavigate} from "react-router-dom";
 
-// const Login = () => {
-//   const navigate = useNavigate();
-//   const signInWithGoogle  = async () => { 
-//   const result = await signInWithPopup(auth, provider)
-//   console.log(result);
-//   navigate("/")
-//   };
-//   return (
-//     <div>
-//     <p> sign in with google to continue</p> 
-//     <button onClick={signInWithGoogle}> Sign In With Google </button>
-//     </div>
-//   ) 
-// }
-
-// export default Login
 import { auth, googleProvider } from "../config/firebase";
 import {
   createUserWithEmailAndPassword,
   signInWithPopup,
-  signOut,
+  // signOut,
 } from "firebase/auth";
 import { useState } from "react";
-
-export const Login = () => {
+import {useNavigate} from "react-router-dom";
+const Login = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -37,6 +18,7 @@ export const Login = () => {
     } catch (err) {
       console.error(err);
     }
+    navigate("/")
   };
 
   const signInWithGoogle = async () => {
@@ -45,18 +27,11 @@ export const Login = () => {
     } catch (err) {
       console.error(err);
     }
-  };
-
-  const logout = async () => {
-    try {
-      await signOut(auth);
-    } catch (err) {
-      console.error(err);
-    }
+    navigate("/")
   };
 
   return (
-    <div>
+<div>
       <input
         placeholder="Email..."
         onChange={(e) => setEmail(e.target.value)}
@@ -69,8 +44,8 @@ export const Login = () => {
       <button onClick={signIn}> Sign In</button>
 
       <button onClick={signInWithGoogle}> Sign In With Google</button>
-
-      <button onClick={logout}> Logout </button>
     </div>
-  );
-};
+  ) 
+}
+
+export default Login
